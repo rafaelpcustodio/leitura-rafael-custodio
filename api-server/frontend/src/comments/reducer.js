98b5c5d0ce88b1e724398e114dcb21edd83fc78b
Rafael Custodio <rafael.custodio@dextra-sw.com>
2018-12-18 13:22:17 -0200

    refactoring: change reducer cases that are not consistent with redux pattern

--- api-server/frontend/src/comments/reducer.js
+++ api-server/frontend/src/comments/reducer.js
@@ -7,8 +7,9 @@ export default function (state = INITIAL_STATE, action) {
         return {...state, value: action.payload.allComments, comment:action.payload.comment }
     case 'COMMENTS_REMOVE':
         const commentIndex = state.value.findIndex(comment => comment.id === action.payload.id)
-        state.value.splice(commentIndex, 1, action.payload)
-        return {...state}
+        const passedValue = [...state.value]
+        passedValue.splice(commentIndex, 1, action.payload)
+        return {...state, value: passedValue}
     case 'COMMENTS_SAVE':
         return {...state, value: state.value.concat(action.payload)}
     case 'COMMENTS_VOTE':
--- api-server/frontend/src/posts/reducer.js
+++ api-server/frontend/src/posts/reducer.js
@@ -2,21 +2,22 @@ const INITIAL_STATE = {value: [], post: {}}
 
 export default function (state = INITIAL_STATE, action) {
     switch(action.type){
-    case 'POSTS_VOTE':///ok??
+    case 'POSTS_VOTE':
         const { payload } = action
         return {...state, value: payload }
-    case 'POSTS_GET_BY_ID'://ok
+    case 'POSTS_GET_BY_ID':
         return {...state, post: action.payload }
-    case 'POSTS_GET_ALL'://ok
+    case 'POSTS_GET_ALL':
         return {...state, value: action.payload}
-    case 'POSTS_GET_ALL_BY_CATEGORY'://ok
+    case 'POSTS_GET_ALL_BY_CATEGORY':
         return {...state, value: action.payload}
-    case 'POSTS_SAVE'://ok
+    case 'POSTS_SAVE':
         return {...state, value: state.value.concat(action.payload)}
-    case 'POSTS_REMOVE'://ok
+    case 'POSTS_REMOVE':
         const postIndex = state.value.findIndex(post => post.id === action.payload.id)
-        state.value.splice(postIndex, 1, action.payload)
-        return {...state }
+        const passedValue = [...state.value]
+        passedValue.splice(postIndex, 1, action.payload)
+        return {...state, value: passedValue }
     case 'POSTS_EDIT':
         return {...state, value: action.payload.allPosts, post:action.payload.post }
     default:
